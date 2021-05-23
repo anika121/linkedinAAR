3d0aa795e072d3e03d9749ad87a838ac198937f5
Aarav <aarav.agarwal.000@gmail.com>
2021-05-23 08:34:11 -0400

    Login page done... Logic needed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
-	return <div className="App">
-		<h1>Linked in 🚀</h1>
-	</div>;
+	return (
+		<div className="App">
+			<Router>
+				<Switch>
+					<Route exact path="/">
+						<Login />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
